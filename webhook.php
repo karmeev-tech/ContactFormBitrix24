a5d55040ea439c838070cc7318143ae447cba734
Kirill Karmeev <70718637+hoo-hunter@users.noreply.github.com>
2021-08-20 21:14:04 +0300

    Complete

--- webhook.php
+++ webhook.php
@@ -1,23 +1,51 @@
 <?php
-echo json_encode(sendDataToBitrix('profile', []), JSON_UNESCAPED_UNICODE);
+    $name = $_POST['name'];
+    $company = $_POST['company'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $subj = $_POST['subj'];
 
- function sendDataToBitrix($method, $data) {
-    $queryUrl = 'https://b24-efglx4.bitrix24.ru/rest/1/w3bdo3i6kmtww5rv/' . $method ;
-    $queryData = http_build_query($data);
+    $queryUrl = 'https://b24-nsad9o.bitrix24.ru/rest/1/r3unctg0vam7omey/user.current.json';
 
+    $queryData = http_build_query(array(
+        'fields' => array(
+    'TITLE' => $company,
+    'NAME' => $name,
+    'EMAIL' => Array(
+           "n0" => Array(
+               "VALUE" => "$email",
+               "VALUE_TYPE" => "WORK",
+           ),
+       ),
+       'PHONE' => Array(
+           "n0" => Array(
+               "VALUE" => "$phone",
+               "VALUE_TYPE" => "WORK",
+           ),
+       ),
+       'COMMENTS' => Array(
+           "n0" => Array(
+               "VALUE" => "$subj",
+           ),
+       ),
+  ),
+        'params' => array("REGISTER_SONET_EVENT" => "Y")
+    ));
+    
+    // обращаемся к Битрикс24 curl_exec
     $curl = curl_init();
     curl_setopt_array($curl, array(
-      CURLOPT_SSL_VERIFYPEER => 0,
-      CURLOPT_POST => 1,
-      CURLOPT_HEADER => 0,
-      CURLOPT_RETURNTRANSFER => 1,
-      CURLOPT_URL => $queryUrl,
-      CURLOPT_POSTFIELDS => $queryData,
+        CURLOPT_SSL_VERIFYPEER => 0,
+        CURLOPT_POST => 1,
+        CURLOPT_HEADER => 0,
+        CURLOPT_RETURNTRANSFER => 1,
+        CURLOPT_URL => $queryUrl,
+        CURLOPT_POSTFIELDS => $queryData,
     ));
-
     $result = curl_exec($curl);
     curl_close($curl);
-    return json_decode($result, 1);
-  }
-  
+    $result = json_decode($result, 1);
+    if (array_key_exists('error', $result)) echo "Ошибка при сохранении лида: ".$result['error_description'].
+    "<br/>";
+
 ?>
